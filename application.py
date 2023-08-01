fc922adc1b9cd1cee7d6f09a57e0db9c363a3f38
Avirat31 <avirats18@gmail.com>
2023-08-02 01:23:07 +0530

    Merge changes from remote main branch

--- application.py
+++ application.py
@@ -8,14 +8,16 @@ application = Flask(__name__)
 app=application
 app.debug = True
 
+
 ## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
-## Route for home page
-@app.route('/')
-def index():
-    return render_template('index.html')
+# ## Route for home page
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
@@ -41,4 +43,4 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0")
